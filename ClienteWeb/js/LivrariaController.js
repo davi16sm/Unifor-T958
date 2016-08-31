1a11071595398b14dfbd02b75cc16f04495a131d
Erick Nilsen Pereira de Souza - DATAPREVCE <erick.nilsen@dataprev.gov.br>
2016-08-31 14:42:39 -0300

    Cliente Web

--- ClienteWeb/js/LivrariaController.js
+++ ClienteWeb/js/LivrariaController.js
@@ -1,5 +1,29 @@
-app.controller('LivrariaController', function($scope, $http) {
+app.controller('LivrariaController', function ($scope, $http) {
+
+    $scope.livro = {};
+    $scope.listaLivros = [];
+
+    $scope.adiciona = function () {
+        $scope.livro.id = $scope.listaLivros.lenght+1;
+
+        $http.post('http://localhost:3000/livros', $scope.livro).success(
+            function (dados) {
+                $scope.listaLivros.push($scope.livro);
+                $scope.livro = {};
+            }
+        );
+    };
 
 
-	
+    $scope.lista = function () {
+
+        $http.get('http://localhost:3000/livros').success(
+            function (dados) {
+                $scope.listaLivros = dados;
+            }
+        );
+    };
+
+    $scope.lista();
+
 });
